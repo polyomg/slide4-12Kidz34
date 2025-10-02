ada29f4b2d65bf2dfd91baef053dd048bc661960
12Kidz34 <phamthituyetnga1111@gmail.com>
2025-10-02 13:32:05 +0700

    TS00777 - VuLongQuan - slide4 - first commit

---
+++ src/main/java/com/poly/slide04/Slide04Application.java
@@ -0,0 +1,13 @@
+package com.poly.slide04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slide04Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slide04Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/poly/slide04/Slide04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poly.slide04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slide04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
